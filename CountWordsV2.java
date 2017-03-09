e7f5dbe5eed25932fedd179c1c542fefba34a5a5
Yakovlev Egor <egor.yakowleff2012@hotmail.com>
2017-03-09 19:07:08 +0300

    Java file added again-again

---
+++ CountWordsV2.java
@@ -0,0 +1,45 @@
+package track.lessons.lesson1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ *
+ */
+
+public class CountWords {
+
+    public long countNumbers(File file) throws Exception {
+        FileReader reader = new FileReader(file);
+        BufferedReader in = new BufferedReader(reader);
+        StringBuilder str = new StringBuilder();
+        int currentNum = 0;
+        String st;
+        while ((st = in.readLine()) != null) {
+            currentNum = currentNum + Integer.parseInt(st);
+        }
+        in.close();
+        return currentNum;
+    }
+
+    public String concatWords(File file) throws Exception {
+        FileReader reader = new FileReader(file);
+        BufferedReader in = new BufferedReader(reader);
+        StringBuilder str = new StringBuilder();
+        int num = 0;
+        String st;
+        while ((st = in.readLine()) != null) {
+            try {
+                num = Integer.parseInt(st);
+            } catch (NumberFormatException e) {
+                if (!"".equals(st.trim())) {
+                    str.append(st.trim() + " ");
+                }
+            }
+        }
+        in.close();
+        return str.toString();
+    }
+
+}
