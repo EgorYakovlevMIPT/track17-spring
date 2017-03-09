03ba8c0633073c1a476a09dcbe89bdfbf78bd23f
Yakovlev Egor <egor.yakowleff2012@hotmail.com>
2017-03-09 18:17:40 +0300

    Java file added

--- src/main/java/track/lessons/lesson1/CountWords.java
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -1,68 +1,45 @@
 package track.lessons.lesson1;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileReader;
 
 /**
- * Задание 1: Реализовать два метода
- *
- * Формат файла: текстовый, на каждой его строке есть (или/или)
- * - целое число (int)
- * - текстовая строка
- * - пустая строка (пробелы)
- *
- *
- * Пример файла - words.txt в корне проекта
- *
- * ******************************************************************************************
- *  Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
- *
- *  Можно дописывать новый код - вспомогательные методы, конструкторы, поля
- *
- * ******************************************************************************************
  *
  */
+
 public class CountWords {
-    /**
-     * Метод на вход принимает объект File, изначально сумма = 0
-     * Нужно пройти по всем строкам файла, и если в строке стоит целое число,
-     * то надо добавить это число к сумме
-     * @param file - файл с данными
-     * @return - целое число - сумма всех чисел из файла
-     */
+
     public long countNumbers(File file) throws Exception {
-        Scanner scn = new Scanner (file);
-        int currentNumber = 0;
-        while (scn.hasNext()) {
-            currentNumber = currentNumber + scn.nextInt();
+        FileReader reader = new FileReader(file);
+        BufferedReader in = new BufferedReader(reader);
+        StringBuilder str = new StringBuilder();
+        int currentNum = 0;
+        String st;
+        while ((st = in.readLine()) != null) {
+            currentNum = currentNum + Integer.parseInt(st);
         }
-        scn.close();
-        return currentNumber;
+        in.close();
+        return currentNum;
     }
 
-
-    /**
-     * Метод на вход принимает объект File, изначально результат= ""
-     * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
-     * то надо присоединить ее к результату через пробел
-     * @param file - файл с данными
-     * @return - результирующая строка
-     */
     public String concatWords(File file) throws Exception {
-        Scanner scn = new Scanner (file);
-        StringBuilder buildString = new StringBuilder ();
-        String currentString = "";
-        while (scn.hasNext()) {
-            buildString.append(scn.next() + " ");
+        FileReader reader = new FileReader(file);
+        BufferedReader in = new BufferedReader(reader);
+        StringBuilder str = new StringBuilder();
+        int num = 0;
+        String st;
+        while ((st = in.readLine()) != null) {
+            try {
+                num = Integer.parseInt(st);
+            } catch (NumberFormatException e) {
+                if (!"".equals(st.trim())) {
+                    str.append(st.trim() + " ");
+                }
+            }
         }
-        scn.close();
-        return currentString;
+        in.close();
+        return str.toString();
     }
-    
-    public static void main (String [] args) throws Exception {
-        File f = new File ("/home/eyakovlev/works/projectsJava/track17-spring/src/main/java/track/lessons/lesson1");
-        CountWords newCW = new CountWords ();
-        System.out.println( newCW.countNumbers(f));
-        System.out.println(newCW.concatWords(f));
-    }
+
 }
